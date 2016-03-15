f3ccbe6bdc0f14ae5ce986fe5ce804005552fc01
Humberto Makoto Morimoto Burgos <humbertomakotomorimoto@gmail.com>
2016-03-14 21:24:00 -0600

    Advanced in Users' register view

--- views/regUsuarios.php
+++ views/regUsuarios.php
@@ -12,7 +12,7 @@ include_once('../elements/header.php');
     ?>
 
     <body>
-    	<div id="registroAlumnos" class="container">
+    	<div id="registroUsuarios" class="container">
 	    	<ul class="nav nav-pills nav-justified">
 				<li class="active"><a data-toggle="pill" href="#Maestros">Maestros</a></li>
 				<li><a data-toggle="pill" href="#Alumnos">Alumnos</a></li>
@@ -20,7 +20,20 @@ include_once('../elements/header.php');
 
 			<div class="tab-content">
 				<div id="Maestros" class="tab-pane fade in active">
-					Maestros
+					<h3 class="text-center">Registro</h3>
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3">
+							<div class="form-group">
+								<label for="sel1">Select list:</label>
+								<select class="form-control" id="sel1">
+							    	<option>1</option>
+							    	<option>2</option>
+							    	<option>3</option>
+							    	<option>4</option>
+							  	</select>
+							  </div>
+						</div>
+					</div>
 				</div>
 				<div id="Alumnos" class="tab-pane fade">
 					Alumnos
