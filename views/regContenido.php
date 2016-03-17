431bcdc3d4076b42d3140638830a77f300df5671
Humberto Makoto Morimoto Burgos <humbertomakotomorimoto@gmail.com>
2016-03-17 13:20:02 -0600

    Finished register view for Subject topics

--- views/regContenido.php
+++ views/regContenido.php
@@ -13,64 +13,100 @@ include_once('../elements/header.php');
     ?>
 
     <body>
-    	<div class="container">
-	    	<h3 class="text-center">Registro</h3>
-			<div class="row">
-				<div class="col-sm-6 col-sm-offset-3">
-					<div class="form-group">
-						<label for="selMateria">Materia:</label>
-						<select class="form-control" id="selMateria">
-					    	<option>Materia 1</option>
-					    	<option>Materia 2</option>
-					    	<option>Materia 3</option>
-					    	<option>Materia 4</option>
-					  	</select>
-				    </div>
-				</div>
-				<div class="col-sm-6 col-sm-offset-3">
-					<div class="form-group">
-						<label for="selTema">Tema:</label>
-						<select class="form-control" id="selTema">
-					    	<option>Tema 1</option>
-					    	<option>Tema 2</option>
-					    	<option>Tema 3</option>
-					    	<option>Tema 4</option>
-					  	</select>
-				    </div>
-				</div>
-				<div class="col-sm-6 col-sm-offset-3">
-					<label>Pregunta:</label>
-					<div class="form-group">
-						<input type="text" class="form-control" id="pregunta"/>
+    	<div id="registroContenido" class="container">
+	    	<ul class="nav nav-pills nav-justified">
+				<li class="active"><a data-toggle="pill" href="#Temas">Temas</a></li>
+				<li><a data-toggle="pill" href="#Preguntas">Preguntas</a></li>
+			</ul>
+
+			<div class="tab-content">
+				<div id="Temas" class="tab-pane fade in active">
+					<h3 class="text-center">Registro</h3>
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3">
+							<div class="form-group">
+								<label for="selMateria">Materia:</label>
+								<select class="form-control" id="selMateria">
+							    	<option>Materia 1</option>
+							    	<option>Materia 2</option>
+							    	<option>Materia 3</option>
+							    	<option>Materia 4</option>
+							  	</select>
+						    </div>
+						</div>
+						<div class="col-sm-6 col-sm-offset-3">
+							<label>Tema:</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="tema"/>
+							</div>
+						</div>
+						<div class="col-sm-2 col-sm-offset-5">
+							<div class="form-group">
+								<button class="btn btn-primary btn-lg" id="crearTema">Crear tema</button>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="col-sm-3 col-sm-offset-3">
-					<label>Opción A (Correcta):</label>
-					<div class="form-group">
-						<input type="text" class="form-control" id="opcionA"/>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<label>Opción B:</label>
-					<div class="form-group">
-						<input type="text" class="form-control" id="opcionB"/>
-					</div>
-				</div>
-				<div class="col-sm-3 col-sm-offset-3">
-					<label>Opción C:</label>
-					<div class="form-group">
-						<input type="text" class="form-control" id="opcionC"/>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<label>Opción D:</label>
-					<div class="form-group">
-						<input type="text" class="form-control" id="opcionD"/>
-					</div>
-				</div>
-				<div class="col-sm-2 col-sm-offset-5">
-					<div class="form-group">
-						<button class="btn btn-primary btn-lg" id="crearPregunta">Crear pregunta</button>
+				<div id="Preguntas" class="tab-pane fade">
+					<h3 class="text-center">Registro</h3>
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3">
+							<div class="form-group">
+								<label for="selMateria2">Materia:</label>
+								<select class="form-control" id="selMateria2">
+							    	<option>Materia 1</option>
+							    	<option>Materia 2</option>
+							    	<option>Materia 3</option>
+							    	<option>Materia 4</option>
+							  	</select>
+						    </div>
+						</div>
+						<div class="col-sm-6 col-sm-offset-3">
+							<div class="form-group">
+								<label for="selTema">Tema:</label>
+								<select class="form-control" id="selTema">
+							    	<option>Tema 1</option>
+							    	<option>Tema 2</option>
+							    	<option>Tema 3</option>
+							    	<option>Tema 4</option>
+							  	</select>
+						    </div>
+						</div>
+						<div class="col-sm-6 col-sm-offset-3">
+							<label>Pregunta:</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="pregunta"/>
+							</div>
+						</div>
+						<div class="col-sm-3 col-sm-offset-3">
+							<label>Opción A (Correcta):</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="opcionA"/>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<label>Opción B:</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="opcionB"/>
+							</div>
+						</div>
+						<div class="col-sm-3 col-sm-offset-3">
+							<label>Opción C:</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="opcionC"/>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<label>Opción D:</label>
+							<div class="form-group">
+								<input type="text" class="form-control" id="opcionD"/>
+							</div>
+						</div>
+						<div class="col-sm-2 col-sm-offset-5">
+							<div class="form-group">
+								<button class="btn btn-primary btn-lg" id="crearPregunta">Crear pregunta</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
